8d6bae5c9a402b052f97dbb1a6663e977114d36c
Skensy <11029252+Skensy@users.noreply.github.com>
2024-11-04 17:00:11 +0100

    added headless ui for testing

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './Navbar.css'
-import logo from './assets/logo-studio-serni-monica.png'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./Navbar.css";
+import logo from "./assets/logo-studio-serni-monica.png";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 function Navbar() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <nav className='flex justify-center items-center flex-wrap justify-around'>
-        <div className='flex items-center w-auto justify-between'>
-          <a href="#"><img className='h-36 min-w-min ' src={logo} alt="logo-studio-serni-monica" /></a>
-          <a href="#" className='no-underline'><h1 className='text-2xl'>STUDIO SERNI MONICA</h1></a>
+      <nav className="flex justify-center items-center flex-wrap justify-around">
+        <div className="flex items-center w-auto justify-between">
+          <a href="#">
+            <img
+              className="h-36 min-w-min "
+              src={logo}
+              alt="logo-studio-serni-monica"
+            />
+          </a>
+          <a href="#" className="no-underline">
+            <h1 className="text-2xl">STUDIO SERNI MONICA</h1>
+          </a>
         </div>
-        <ul className='flex gap-8 text-lg justify-center'>
-          <a href="#"><li>Home</li></a>
-          <a href="#"><li>Contatti</li></a>
-          <a href="#"><li>Servizi</li></a>
-          <a href="#"><li>FAQ</li></a>
-        </ul>
+        <div>
+          <Menu>
+            <MenuButton className="w-36">MENU</MenuButton>
+            <MenuItems anchor="bottom" className="w-36">
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 bg-white" href="/settings">
+                  Home
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 bg-white" href="/support">
+                  Contatti
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 bg-white" href="/license">
+                  Servizi
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a className="block data-[focus]:bg-blue-100 bg-white" href="#">
+                  FAQ
+                </a>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/Slider.jsx
+++ src/Slider.jsx
@@ -5,12 +5,15 @@ function Slider() {
 
   return (
     <>
+    <div className="">
       <div className="w-full h-96 w-auto flex items-center bg-gradient-to-r from-cyan-500 to-blue-500 justify-center mt-4">
         <div>
           <div className="text-4xl text-white">SERNI MONICA</div>
           <div className="text-white">consulente del lavoro</div>
         </div>
       </div>
+    </div>
+
     </>
   );
 }
