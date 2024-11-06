18ad83c20d15b3f4e1a9b7926822303ba7001eaa
Skensy <11029252+Skensy@users.noreply.github.com>
2024-11-06 11:13:11 +0100

    added nextui

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
 import "./index.css";
 import Navbar from "./Navbar.jsx";
 import Slider from "./Slider.jsx";
 import Midcontent from "./Midcontent.jsx";
 import Card from "./Card.jsx";
 import Paghe from "./assets/paghe-e-contributi.jpg";
-import Assistenza from "./assets/assistenza-fiscale-serni-monica.avif"
-import Consulenza from "./assets/consulenza-contrattuale-e-del-lavoro.avif"
-import Gestione from "./assets/gestione-delle-risorse-umane.avif"
+import Assistenza from "./assets/assistenza-fiscale-serni-monica.avif";
+import Consulenza from "./assets/consulenza-contrattuale-e-del-lavoro.avif";
+import Gestione from "./assets/gestione-delle-risorse-umane.avif";
 import Cards from "./CardLists.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Navbar />
-    <Slider title="STUDIO SERNI MONICA" subtitle="consulente del lavoro" gradient="" imgUrl="./src/assets/slider-foto.avif" />
-    <Midcontent />
-    <Cards />
+    <NextUIProvider>
+      <Navbar />
+      <Slider
+        title="STUDIO SERNI MONICA"
+        subtitle="consulente del lavoro"
+        gradient=""
+        imgUrl="./src/assets/slider-foto.avif"
+      />
+      <Midcontent />
+      <Cards />
+    </NextUIProvider>
   </StrictMode>
 );
