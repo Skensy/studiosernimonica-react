60fd35885c0ffd1a46f0b9646a717273276bf6d6
Skensy <11029252+Skensy@users.noreply.github.com>
2024-11-07 15:46:13 +0100

    added react router dom

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,9 +1,7 @@
 import sliderFoto from "./assets/slider-foto.avif";
 import { motion } from "framer-motion";
 
-
 function Slider(props) {
-
   const title = props.title;
   const subtitle = props.subtitle;
   const gradient = props.gradient;
@@ -11,16 +9,26 @@ function Slider(props) {
 
   return (
     <>
-    <div className="flex flex-col w-full relative shadow-xl">
-      <div className="flex flex-col justify-center items-center">
-        <div className={`h-dvh flex w-full items-center ${gradient} justify-center z-0 bg-cover bg-center)`} style={{ backgroundImage: `url('${imgUrl}')` }} />
-        <div className="h-dvh flex w-full z-10 bg-black/60 absolute" />
-        <div className="flex flex-col z-20 absolute">
-          <div className="text-4xl text-white font-bold text-center ">{title}</div>
-          <div className="text-white text-2xl text-center">{subtitle}</div>
+      <div className="flex flex-col w-full relative shadow-xl">
+        <div className="flex flex-col justify-center items-center">
+          <div
+            className={`h-dvh flex w-full items-center ${gradient} justify-center z-0 bg-cover bg-center)`}
+            style={{ backgroundImage: `url('${imgUrl}')` }}
+          />
+          <div className="h-dvh flex w-full z-10 bg-black/60 absolute" />
+          <div className="flex flex-col z-20 absolute">
+            <motion.div
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 1.1 }}
+            >
+              <div className="text-4xl text-white font-bold text-center ">
+                {title}
+              </div>
+              <div className="text-white text-2xl text-center">{subtitle}</div>
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
