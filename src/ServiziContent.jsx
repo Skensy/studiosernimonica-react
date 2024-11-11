fd8d553d980470fa844bb2f3b2fb9ac8a80bed2d
Skensy <11029252+Skensy@users.noreply.github.com>
2024-11-11 11:05:22 +0100

    changed padding on servizicontent

--- src/ServiziContent.jsx
+++ src/ServiziContent.jsx
@@ -3,8 +3,8 @@ import "./index.css"
 
 function ServiziContent() {
   return (
-    <div className="my-36 px-24 sm:px-12 md:px-36 lg:px-48 xl:px-96 py-12 text-black">
-      <motion.p className="font-light text-2xl text-center">
+    <div className="my-36 px-12 sm:px-12 md:px-36 lg:px-48 xl:px-96 py-12 text-black">
+      <motion.p className="font-light text-xl sm:md:lg:xl:text-2xl text-center">
         Lo Studio Serni Monica offre una vasta gamma di servizi di consulenza
         del lavoro, pensati per rispondere alle esigenze specifiche di aziende
         di ogni dimensione e settore. La nostra esperienza e competenza ci
